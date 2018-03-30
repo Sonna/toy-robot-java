6806241ed52047f05daf30f00045e2e06ccbea9f
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-30 23:12:01 +1100

    Add constructor to Robot class
    Add multiple constructors to Robot class to build the Robot with or
    without multiple parameters. By default a Robot will be set with `x`,
    `y` and `facing` coordinates at `0`, `0`, and `"NORTH"` respectively.
    
    == References:
    - [Java Object and Classes]
      (https://www.tutorialspoint.com/java/java_object_classes.htm)

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,8 +1,32 @@
 package com.sonneveld.ToyRobot;
 
 public class Robot {
-    public boolean someLibraryMethod() {
-        return true;
+    int x;
+    int y;
+    String facing;
+
+    public Robot() {
+        this.x = 0;
+        this.y = 0;
+        this.facing = "NORTH";
+    }
+
+    public Robot(int x) {
+        this.x = x;
+        this.y = 0;
+        this.facing = "NORTH";
+    }
+
+    public Robot(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.facing = "NORTH";
+    }
+
+    public Robot(int x, int y, String facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
     }
 
     public void report() {
--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -4,8 +4,31 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RobotTest {
-    @Test public void testSomeLibraryMethod() {
-        Robot classUnderTest = new Robot();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    @Test public void testConstructorNoArguments() {
+        Robot subject = new Robot();
+        assertEquals(0, subject.x);
+        assertEquals(0, subject.y);
+        assertEquals("NORTH", subject.facing);
+    }
+
+    @Test public void testConstructorOneArguments() {
+        Robot subject = new Robot(1);
+        assertEquals(1, subject.x);
+        assertEquals(0, subject.y);
+        assertEquals("NORTH", subject.facing);
+    }
+
+    @Test public void testConstructorTwoArguments() {
+        Robot subject = new Robot(1, 2);
+        assertEquals(1, subject.x);
+        assertEquals(2, subject.y);
+        assertEquals("NORTH", subject.facing);
+    }
+
+    @Test public void testConstructorThreeArguments() {
+        Robot subject = new Robot(1, 2, "SOUTH");
+        assertEquals(1, subject.x);
+        assertEquals(2, subject.y);
+        assertEquals("SOUTH", subject.facing);
     }
 }
